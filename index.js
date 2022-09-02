d2fd730885517435ec689259281f4875ab961db3
unknown <julianatonu@gmail.com>
2022-09-02 21:15:24 +0600

    add Navbar

---
+++ index.js
@@ -0,0 +1,12 @@
+const loadNews =async()=>{
+    const url=`https://openapi.programming-hero.com/api/news/category/01`
+    const res =await fetch(url)
+    const data =await res.json()
+    displayNews(data.data)
+}
+
+const displayNews =(news)=>{
+console.log(news)
+}
+
+loadNews('');

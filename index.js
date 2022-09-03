043c71bb878782241c5789ebfbd6615c790702fd
unknown <julianatonu@gmail.com>
2022-09-03 14:22:05 +0600

    show items number for each category

--- index.js
+++ index.js
@@ -9,39 +9,44 @@ const displayNews =(news)=>{
 console.log(news)
 console.log('news Length', news.length)
 
-toggleSpinner(true)
+
 const newsContainer =document.getElementById('news-container')
 newsContainer.textContent=''
 
-//news item
-
+//news category item
+                                                   
 const newsField =document.getElementById('news-item-field')
-const newsText=newsField.value 
-newsText.value =news.length
-console.log('please news', newsText.value)
+newsField.innerHTML=`
+<p class="px-3  fw-semibold">${news.length} Item's founded for this category </p>
+`
+
 
 //no message found
 const noNews =document.getElementById('no-found-message')
 if(news.length === 0){
 noNews.classList.remove('d-none')
+toggleSpinner(true)
 }
 else{
   noNews.classList.add('d-none')
-
+  toggleSpinner(false)
 }
+
+
+
 news.forEach(mynews => {
     console.log(mynews)
 
 const {title, thumbnail_url,details,total_view,_id}=mynews
-const {name,published_date,img} =mynews.author
+const {name,img} =mynews.author
     const newsDiv =document.createElement('div')
     newsDiv.innerHTML =`
-    <div class="card mt-3 mx-auto" style="max-width: 900px;">
+    <div class="card mt-3 mx-auto" style="max-width: 800px;">
     <div class="row g-0 ">
     <div class="col-md-4 ">
       <img src="${thumbnail_url}" class="img-fluid rounded-start h-100" alt="...">
     </div>
-    <div class="col-md-8">
+    <div class="col-md-8" col-sm-4 >
       <div class="card-body">
         <h5 class="card-title">${title}</h5>
         <p class="card-text">${details.slice(0,150)}...</p>
@@ -53,19 +58,19 @@ const {name,published_date,img} =mynews.author
             <div class="col-2">
                 <img src="${img}" class="  author"  alt="...">
             </div>
-            <div class="col-10">
+            <div class="col-4">
                 <p>${name}</p>
-                <p>${published_date}</p>
+                
             </div>
             
             
         </div>
         
     </div>
-    <div class="col">
+    <div class="col-4">
         <div class="row">
         <div class="col d-row">
-        <p>Total views: <span>${total_view}</span></p>
+        <p><i class="fa-regular fa-eye"></i> <span>${total_view}</span></p>
     </div>
         
 
@@ -74,7 +79,7 @@ const {name,published_date,img} =mynews.author
       </div>
     </div>
   </div>
-  <div class="text-center "><button onclick="newsDetails('${_id}')" class="btn btn-warning px-5" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button></div>
+  <div class=" "><button onclick="newsDetails('${_id}')" class="btn btn-primary px-3 ms-4" data-bs-toggle="modal" data-bs-target="#exampleModal">See Details</button></div>
   
 
           </div>
@@ -111,14 +116,15 @@ const displayNewsDetails =(news)=>{
     <p class="card-text">${news[0].details.slice(0,300)}</p>
     
     <div class="row">
-    <div class="col ">
+    <div class="col col-sm-12">
         <div class="row text-left">
             <div class="col-2">
                 <img src="${news[0].author.img}" class="  author"  alt="...">
             </div>
-            <div class="col-10">
+            <div class="col-10" >
                 <p>${news[0].author.name ? news[0].author.name :'not found' }</p>
                 <p>${news[0].author.published_date}</p>
+                <p><i class="fa-regular fa-eye"></i> <span>${news[0].total_view ? news[0].total_view :"Not found"}</span></p>
             </div>
             
             
@@ -127,8 +133,8 @@ const displayNewsDetails =(news)=>{
     </div>
     <div class="col">
         <div class="row">
-        <div class="col d-row">
-        <p>Total views: <span>${news[0].total_view ? news[0].total_view :"Not found"}</span></p>
+        <div class="col ms-4 ps-5">
+        
     </div>
         
 
@@ -164,33 +170,33 @@ const displayCategories=(category)=>{
         categoryNews.innerHTML=`
         
         <li  class="nav-item px-4">
-        <a onclick="loadNews('01')" class="nav-link active" href="#">${category[0].category_name}</a>
+        <a onclick="loadNews('01')" class="nav-link active  text-primary fw-semibold"  href="#">${category[0].category_name}</a>
       </li>
         <li  class="nav-item px-4">
-        <a  onclick="loadNews('02')" class="nav-link active" href="#">${category[1].category_name}</a>
+        <a  onclick="loadNews('02')" class="nav-link active text-primary fw-semibold" href="#">${category[1].category_name}</a>
       </li>
         <li  class="nav-item px-4">
-        <a  onclick="loadNews('03')" class="nav-link active" href="#">${category[2].category_name}</a>
+        <a  onclick="loadNews('03')" class="nav-link active text-primary fw-semibold" href="#">${category[2].category_name}</a>
       </li>
         <li  class="nav-item px-4">
-        <a  onclick="loadNews('04')" class="nav-link active" href="#">${category[3].category_name}</a>
+        <a  onclick="loadNews('04')" class="nav-link active text-primary fw-semibold" href="#">${category[3].category_name}</a>
       </li>
         <li  class="nav-item px-4">
-        <a onclick="loadNews('05')" class="nav-link active" href="#">${category[4].category_name}</a>
+        <a onclick="loadNews('05')" class="nav-link active text-primary fw-semibold" href="#">${category[4].category_name}</a>
       </li>
         <li  class="nav-item px-4">
-        <a onclick="loadNews('06')" class="nav-link active" href="#">${category[5].category_name}</a>
+        <a onclick="loadNews('06')" class="nav-link active text-primary fw-semibold" href="#">${category[5].category_name}</a>
       </li>
         <li  class="nav-item px-4">
-        <a onclick="loadNews('07')" class="nav-link active" href="#">${category[6].category_name}</a>
+        <a onclick="loadNews('07')" class="nav-link active text-primary fw-semibold" href="#">${category[6].category_name}</a>
       </li>
         <li  class="nav-item px-4">
-        <a onclick="loadNews('08')"  class="nav-link active" href="#">${category[7].category_name}</a>
+        <a onclick="loadNews('08')"  class="nav-link active text-primary fw-semibold " href="#">${category[7].category_name}</a>
       </li>
         `
     });
   
-    
+   
 }
 // spinner
 const toggleSpinner =isLoading =>{
@@ -205,6 +211,8 @@ const toggleSpinner =isLoading =>{
 
   }
 }
+
+
 newsCategories()
 
 
